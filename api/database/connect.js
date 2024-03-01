e955ad06240c3fb7b273de28be9f92e01fe570dd
DIPAK JAWKAR <120739704+Dipakjawkar@users.noreply.github.com>
2024-03-02 02:36:14 +0530

    Update connect.js
    add process.env

--- api/database/connect.js
+++ api/database/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = async ()=>{
-    const con = await mongoose.connect('mongodb://localhost:27017/blog')
+    const con = await mongoose.connect(process.env.DB_CONNECT)
     con ? console.log("DB is Conneted") : console.log("DB Conneted Problam ")
 }
 connect()
