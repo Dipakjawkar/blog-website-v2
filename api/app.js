1d72bce0aff82fae48b664234e494db4154f59d2
DIPAK JAWKAR <120739704+Dipakjawkar@users.noreply.github.com>
2024-03-04 14:31:12 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const path = require('path')
+
 
 require('dotenv').config();
 require('./database/connect')
